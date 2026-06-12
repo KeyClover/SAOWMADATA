2c37f19b07ba1d3bf54d3accfc68b45f002439a7
KeyClover <suvichak0001@gmail.com>
2026-06-12 21:58:45 +0700

    Update base and homepage for KeyClover/SAOWMADATA repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // base: "./" ทำให้ใช้ได้กับ GitHub Pages ทุกชื่อ repo โดยไม่ต้องตั้งค่าเพิ่ม
 export default defineConfig({
   plugins: [react()],
-  base: "/SAOWMADATA.github.io/",
+  base: "/SAOWMADATA/",
 });
